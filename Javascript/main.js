ed2c2e72f4cb2993c88a90e2d0c0b536734cfb73
Iretii0511 <simonjulius0511@gmail.com>
2020-07-25 23:24:34 +0100

    Landing page is completely done

--- Javascript/main.js
+++ Javascript/main.js
@@ -14,12 +14,45 @@ fetchl();
 const createList = data => {
     for (let element of data) {
         let listItem = document.createElement('li');
+        listItem.classList.add('character');
         let button = document.createElement('button');
         button.classList.add('btn');
         button.textContent = "Profile";
         listItem.textContent = `${element['name']}`;
         listItem.appendChild(button);
         list.appendChild(listItem);
+        let closedButton = 'close-button';
+        const span = document.createElement('span')
+        let user = new User(element.name, element.gender, element.height);
+        button.addEventListener('click', ()=>{
+            const div1 = document.createElement('div');
+            // div1.style.width = 100%
+            const div2 = document.createElement('div');
+            const ul = document.createElement('ul');
+            const li1 = document.createElement('li');
+            const li2 = document.createElement('li');
+            const li3 = document.createElement('li');
+            let modal;
+            li1.textContent = user.name;
+            li2.textContent = user.gender;
+            li3.textContent = user.height;
+            ul.appendChild(li1);
+            ul.appendChild(li2);
+            ul.appendChild(li3);
+            div1.classList.add('modal');
+            div1.style.display = 'block';
+            div1.id = 'modal-id';
+            div1.appendChild(div2);
+            div2.classList.add('modal-content');
+            div2.appendChild(span);
+            div2.appendChild(ul);
+            span.classList.add(closedButton);
+            span.textContent = 'x';
+            button.parentElement.appendChild(div1);
+            span.addEventListener('click', () => {
+                listItem.removeChild(div1);
+            })
+        })
     }
 }
 
